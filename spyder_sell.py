d4a1c1dc89b07a65c6a12b1c3109fa45fcaae038
chankein <chankeinjp@yahoo.co.jp>
2020-06-29 22:55:26 +0900

    modify csv file

--- spyder_sell.py
+++ spyder_sell.py
@@ -9,6 +9,7 @@ import pandas as pd
 from pandas import Series, DataFrame
 from file import File
 from send_wechat_message import send_message
+import time
 #URL（ここにURLを入れてください）
 #url = 'https://suumo.jp/jj/bukken/ichiran/JJ010FJ001/?ar=030&bs=021&ta=13&jspIdFlg=patternShikugun&sc=13101&kb=1&kt=9999999&tb=0&tt=9999999&hb=0&ht=9999999&ekTjCd=&ekTjNm=&tj=0&cnb=0&cn=9999999&srch_navi=1'
 #nerima_url = 'https://suumo.jp/jj/bukken/ichiran/JJ010FJ001/?ar=030&bs=021&ta=13&jspIdFlg=patternShikugun&sc=13101&sc=13120&kb=1&kt=9999999&tb=0&tt=9999999&hb=0&ht=9999999&ekTjCd=&ekTjNm=&tj=0&cnb=0&cn=9999999&srch_navi=1'
@@ -87,35 +88,6 @@ class HouseArea:
                     biuild_date = this_value.replace('\n', '')
                 else:
                     pass
-
-
-
-
-
-            # house_price = house_detail.find(
-            #     'span', class_='dottable-value').text
-            # if house_price==None:
-            #     if '円' in name.split(' ')[-1]:
-            #         house_price = name.split(' ')[-1]
-            # else:
-            #     house_price = house_price.replace('\n', '')
-
-            # house_locate_detail = house_detail.find_all(
-            #     'div', class_='dottable-line')[1].find_all('dd')
-
-            # address = house_locate_detail[0].text.replace('\n', '')
-            # if len(house_locate_detail) == 1:
-            #     # print(house_locate_detail[0].text)
-            #     location = house_detail.find_all(
-            #         'div', class_='dottable-line')[0].find_all('dd')[0].text.replace('\n', '')
-            # else:
-            #     location = house_locate_detail[1].text.replace('\n', '')
-
-            # house_detail_in = house_detail.find_all('table')
-            # land_space = house_detail_in[0].find_all('dd')[0].text
-            # building_space = house_detail_in[1].find_all('dd')[0].text
-            # floor_plans = house_detail_in[0].find_all('dd')[1].text
-            # biuild_date = house_detail_in[1].find_all('dd')[1].text
             if house.find('span', class_='makermore-tel-txt'):
                 tel = house.find('span', class_='makermore-tel-txt').text
             else:
@@ -157,40 +129,6 @@ class HouseArea:
         suumo_df_pages.columns = ['マンション名', '販売価格', '住所', '立地', '土地面積',
                                 '建物面積',  '間取り', '築年月', '詳細URL', '電話']
         self.suumo_df_list.append(suumo_df_pages)
-        #return suumo_df_pages
-
-
-
-    # def main(area):
-    #     url = domain + area_url_json['secondHandHouse'][area]
-    #     suumo_df_list = []
-    #     result = requests.get(url)
-    #     c = result.content
-
-    #     soup = BeautifulSoup(c, "lxml")
-
-    #     summary = soup.find("div",{'id':'js-bukkenList'})
-    #     houses = summary.find_all(
-    #         "div", {'class': 'property_unit-content'})
-
-    #     suumo_df_list.append(each_page('', houses))
-
-    #     body = soup.find("body")
-    #     pages = body.find_all("div",{'class':'pagination pagination_set-nav'})
-
-    #     next_pages=pages[0].find_all('a')
-
-    #     if len(next_pages) == 0:
-    #         pass
-    #     else:
-    #         max_pages = int(next_pages[-2].text)
-    #         for x in range(2, max_pages+1):
-    #             pages_url = url + 'pnz1{}.html'.format(str(x))
-    #             suumo_df_list.append(each_page(pages_url, houses=None))
-    #             time.sleep(3)
-    #     suumo_df = pd.concat(suumo_df_list)
-
-    #     suumo_df.to_csv(area + '_old_house.csv', encoding='utf-16', header=True, index=False)
 
 
 if __name__=='__main__':
@@ -199,6 +137,7 @@ if __name__=='__main__':
     else:
         area_url_json['secondHandHouse'] = {"千代田区": "/chukoikkodate/tokyo/sc_chiyoda/",
             "練馬区": "/chukoikkodate/tokyo/sc_nerima/"}
+    all_area_list=[]
     for (area, url) in area_url_json['secondHandHouse'].items():
         AREACLASS = HouseArea(area)
         AREACLASS.each_page(AREACLASS.main_url)
@@ -213,10 +152,13 @@ if __name__=='__main__':
                     AREACLASS.each_page(pages_url)
                     time.sleep(3)
         suumo_df = pd.concat(AREACLASS.suumo_df_list)
-
-        suumo_df.to_csv(area + '_old_house.csv', encoding='utf-16', header=True, index=False)
+        suumo_df['区域'] = area
+        suumo_df['日付'] = time.strftime("%Y-%m-%d", time.localtime(time.time()))
+        all_area_list.append(suumo_df)
         time.sleep(5)
-
+    result_df=pd.concat(all_area_list)
+    result_df.to_csv('/home/ubuntu/streamlitApp/old_house.csv',
+                     encoding='utf-16', header=True, index=False)
 
 #main('千代田区', 'https://suumo.jp/chukoikkodate/tokyo/sc_chiyoda/')
 #for (area, url) in area_url_json['secondHandHouse'].items():
