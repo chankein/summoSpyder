bfe6786820c542e2bfe559e8bd0d336e92d211c1
chankein <chankeinjp@yahoo.co.jp>
2020-07-29 22:40:39 +0900

    update

--- analyse_old_house.py
+++ analyse_old_house.py
@@ -51,6 +51,7 @@ class do_caltulate:
         self.df_target = self.df_target[self.df_target['建物面積(m2)'] > 0]
 
         self.df_target['販売価格(万円)m2'] = self.df_target.apply(lambda x: x['販売価格(万円)'] / x['建物面積(m2)'], axis=1)
+    def grouping(self):
         #单物件均价
         self.avg_price = self.df_target.groupby(['日付', '区域'])['販売価格(万円)'].mean().reset_index()
         
@@ -69,26 +70,41 @@ class do_caltulate:
         self.avg_houses_ages_dis = self.df_target.groupby(['日付', '区域','築年数']).size().reset_index()
         self.avg_houses_ages_dis.columns=['日付', '区域', '築年数', '楼龄分布']
 
+        #各区楼龄价格分布
+        self.age_price_dis = self.df_target.groupby(['区域','築年数'])['販売価格(万円)m2'].mean().reset_index()
+
+        #各区距离价格分布
+        self.distance_price = self.df_target.groupby(['区域','距離(分)'])['販売価格(万円)m2'].mean().reset_index()
+
 
     def write_csv(self,directory):
-        self.avg_price.to_csv(directory + 'avg_price.csv',encoding='utf-16', header=True, index=False)
-        self.avg_price_per_m2.to_csv(directory + 'avg_price_per_m2.csv',encoding='utf-16', header=True, index=False)
-        self.count_houses.to_csv(directory + 'count_houses.csv',encoding='utf-16', header=True, index=False)
-        self.avg_houses_ages.to_csv(directory + 'avg_houses_ages.csv',encoding='utf-16', header=True, index=False)
-        self.avg_houses_ages_dis.to_csv(directory + 'avg_houses_ages_distribute.csv',encoding='utf-16', header=True, index=False)
+        # self.avg_price.to_csv(directory + 'avg_price.csv',encoding='utf-16', header=True, index=False)
+        # self.avg_price_per_m2.to_csv(directory + 'avg_price_per_m2.csv',encoding='utf-16', header=True, index=False)
+        # self.count_houses.to_csv(directory + 'count_houses.csv',encoding='utf-16', header=True, index=False)
+        # self.avg_houses_ages.to_csv(directory + 'avg_houses_ages.csv',encoding='utf-16', header=True, index=False)
+        # self.avg_houses_ages_dis.to_csv(directory + 'avg_houses_ages_distribute.csv',encoding='utf-16', header=True, index=False)
 
+        self.age_price_dis.to_csv(directory + 'avg_houses_ages_price_distribute.csv',encoding='utf-16', header=True, index=False)
+        self.distance_price.to_csv(directory + 'avg_distance_price_distribute.csv',encoding='utf-16', header=True, index=False)
 
-    def regression(self):
+    def get_location(self):
         df_split1=self.df_target['立地'].str.split('「', expand=True)
         df_split2=df_split1[1].str.split('」', expand=True)
         df_split1=df_split1.loc[:,[0]]
         df_split1.columns=['鉄道']
         df_split2.columns=['駅','距離']
-        df_target=pd.concat([self.df_target,df_split1,df_split2], axis=1)
-        df_target['距離(分)']=df_target['距離'].apply(lambda x: get_num(x))
-        df_regression=df_target.loc[:,['建物面積(m2)','土地面積(m2)','築年数','駅','距離(分)','販売価格(万円)']]
-        X=df_regression.loc[:,['建物面積(m2)','土地面積(m2)','築年数','距離(分)']]
-        y=df_regression['販売価格(万円)']
+        self.df_target=pd.concat([self.df_target,df_split1,df_split2], axis=1)
+        self.df_target['距離(分)']=self.df_target['距離'].apply(lambda x: get_num(x))
+
+        
+
+        
+        
+    
+    def regression(self):
+        self.df_regression=self.df_target.loc[:,['建物面積(m2)','土地面積(m2)','築年数','駅','距離(分)','販売価格(万円)']]
+        X=self.df_regression.loc[:,['建物面積(m2)','土地面積(m2)','築年数','距離(分)']]
+        y=self.df_regression['販売価格(万円)']
 
         X=preprocessing.scale(X)
         y=preprocessing.scale(y)
@@ -111,5 +127,8 @@ class do_caltulate:
 
 
 this_analyse = do_caltulate()
-this_analyse.regression()
-# this_analyse.write_csv('./')
+this_analyse.get_location()
+this_analyse.grouping()
+this_analyse.write_csv('./')
+# this_analyse.regression()
+
